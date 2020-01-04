8e86faae01f3a10852c5b8426705851dcb761f5b
samplecm <56743484+samplecm@users.noreply.github.com>
2020-01-04 08:12:20 -0800

    Add files via upload

--- Snake.py
+++ Snake.py
@@ -9,17 +9,17 @@ from tkinter import messagebox
 class Cube(object):
 	rows = 20
 	w = 500
-	def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+	def __init__(self,start,xDir=1,yDir=0,color=(255,0,0)):
 		self.pos = start
-		self.dirnx = 1
-		self.dirny = 0
+		self.xDir = 1
+		self.yDir = 0
 		self.color = color
 		
 		
-	def move(self,dirnx,dirny):
-		self.dirnx = dirnx
-		self.dirny = dirny
-		self.pos = (self.pos[0]+self.dirnx,self.pos[1]+self.dirny)
+	def move(self,xDir,yDir):
+		self.xDir = xDir
+		self.yDir = yDir
+		self.pos = (self.pos[0]+self.xDir,self.pos[1]+self.yDir)
 		   
 		
 		
@@ -49,8 +49,8 @@ class Snake(object):
 		self.color = color
 		self.head = Cube(pos)
 		self.body.append(self.head)
-		self.dirnx = 0
-		self.dirny = 1
+		self.xDir = 0
+		self.yDir = 1
 		
 	def move(self):
 		for event in pygame.event.get():
@@ -59,21 +59,21 @@ class Snake(object):
 			keys = pygame.key.get_pressed()
 			for key in keys:
 				if keys[pygame.K_LEFT]:
-					self.dirnx = -1
-					self.dirny = 0
-					self.turns[self.head.pos[:]]=[self.dirnx,self.dirny] #new turn at this positions
+					self.xDir = -1
+					self.yDir = 0
+					self.turns[self.head.pos[:]]=[self.xDir,self.yDir] #new turn at this positions
 				elif keys[pygame.K_RIGHT]:
-					self.dirnx = 1
-					self.dirny = 0
-					self.turns[self.head.pos[:]]=[self.dirnx,self.dirny]
+					self.xDir = 1
+					self.yDir = 0
+					self.turns[self.head.pos[:]]=[self.xDir,self.yDir]
 				elif keys[pygame.K_UP]:
-					self.dirnx = 0
-					self.dirny = -1
-					self.turns[self.head.pos[:]]=[self.dirnx,self.dirny]
+					self.xDir = 0
+					self.yDir = -1
+					self.turns[self.head.pos[:]]=[self.xDir,self.yDir]
 				elif keys[pygame.K_DOWN]:
-					self.dirnx = 0
-					self.dirny = 1
-					self.turns[self.head.pos[:]]=[self.dirnx,self.dirny]
+					self.xDir = 0
+					self.yDir = 1
+					self.turns[self.head.pos[:]]=[self.xDir,self.yDir]
 		 			
 		for i,c in enumerate(self.body):#look through list of positions (index and cube)
 			p = c.pos[:] #copy so position doesn't get changed
@@ -83,11 +83,11 @@ class Snake(object):
 				if i == len(self.body)-1: #if on last cube, remove turn
 					self.turns.pop(p)
 			else: #of at edges of screen, loop back to other side.
-				if c.dirnx == -1 and c.pos[0] <=0: c.pos = (c.rows-1,c.pos[1])
-				elif c.dirnx ==1 and c.pos[0] >= rows-1: c.pos = (0,c.pos[1])
-				elif c.dirny == 1 and c.pos[1]>=rows-1: c.pos = (c.pos[0],0)
-				elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
-				else: c.move(c.dirnx,c.dirny)
+				if c.xDir == -1 and c.pos[0] <=0: c.pos = (c.rows-1,c.pos[1])
+				elif c.xDir ==1 and c.pos[0] >= rows-1: c.pos = (0,c.pos[1])
+				elif c.yDir == 1 and c.pos[1]>=rows-1: c.pos = (c.pos[0],0)
+				elif c.yDir == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+				else: c.move(c.xDir,c.yDir)
 				
 				 
 			
@@ -96,12 +96,12 @@ class Snake(object):
 		self.body = []
 		self.body.append(self.head)
 		self.turns = {}
-		self.dirnx = 1
-		self.dirny = 0
+		self.xDir = 1
+		self.yDir = 0
 		
 	def addCube(self):
 		tail = self.body[-1]
-		dx,dy = tail.dirnx,tail.dirny
+		dx,dy = tail.xDir,tail.yDir
 		#check direction of movement, to know where to add cube.
 		if dx == 1 and dy == 0:
 			self.body.append(Cube((tail.pos[0]-1,tail.pos[1])))
@@ -112,8 +112,8 @@ class Snake(object):
 		elif dx ==0 and dy == -1:
 			self.body.append(Cube((tail.pos[0],tail.pos[1]+1)))
 		
-		self.body[-1].dirnx = dx
-		self.body[-1].dirny = dy
+		self.body[-1].xDir = dx
+		self.body[-1].yDir = dy
 		
 		
 	def draw(self,surface):
